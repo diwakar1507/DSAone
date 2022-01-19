8b47a51b7f6db3c83ff65c544763490fae21bd8c
Ishan Shukla <ishanshukla05@gmail.com>
2022-01-19 18:15:02 +0530

    Animations Updated

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -9,7 +9,7 @@ import illustration2 from "../../img/Landing Page/Section 3/Illustration.svg";
 import illustration3 from "../../img/Landing Page/Section 4/Illustration.svg";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { gsap } from "gsap";
-import { Link as SLink } from 'react-scroll';
+import { Link as SLink } from "react-scroll";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -27,10 +27,25 @@ const LandingPage = () => {
   const frontRef = useRef(null);
 
   // Section2 Refs
+  const illustration1Ref = useRef(null);
+  const Container1Ref = useRef(null);
+  const Heading1Ref = useRef(null);
+  const Button1Ref = useRef(null);
+  const Data1Ref = useRef(null);
 
   // Section3 Refs
+  const illustration2Ref = useRef(null);
+  const Container2Ref = useRef(null);
+  const Heading2Ref = useRef(null);
+  const Button2Ref = useRef(null);
+  const Data2Ref = useRef(null);
 
   // Section4 Refs
+  const illustration3Ref = useRef(null);
+  const Container3Ref = useRef(null);
+  const Heading3Ref = useRef(null);
+  const Button3Ref = useRef(null);
+  const Data3Ref = useRef(null);
 
   const scrollAnimations = useCallback(() => {
     gsap.to(topbarRef.current, {
@@ -48,10 +63,9 @@ const LandingPage = () => {
     gsap.to(dsaRef.current, {
       scrollTrigger: {
         trigger: dsaRef.current,
-        start: "center 5%",
+        start: "center 10%",
         scrub: true,
       },
-      x: 10,
       duration: 0.6,
       ease: "Power1.out",
       opacity: 0,
@@ -60,10 +74,9 @@ const LandingPage = () => {
     gsap.to(lineRef.current, {
       scrollTrigger: {
         trigger: lineRef.current,
-        start: "center 5%",
+        start: "center 40%",
         scrub: true,
       },
-      x: 10,
       duration: 0.6,
       ease: "Power1.out",
       opacity: 0,
@@ -72,7 +85,7 @@ const LandingPage = () => {
     gsap.to(oneRef.current, {
       scrollTrigger: {
         trigger: oneRef.current,
-        start: "center 40%",
+        start: "center 45%",
         scrub: true,
       },
       duration: 0.3,
@@ -86,8 +99,8 @@ const LandingPage = () => {
         start: "center 50%",
         scrub: true,
       },
-      duration: 0.2,
-      x: 60,
+      duration: 0.1,
+      // x: 60,
       opacity: 0,
       ease: "Power1.out",
     });
@@ -133,6 +146,177 @@ const LandingPage = () => {
       duration: 2,
       y: 20,
       ease: "Power1.out",
+    });
+
+    // Section 2
+    gsap.from(illustration1Ref.current, {
+      scrollTrigger: {
+        trigger: illustration1Ref.current,
+        start: "top 75%",
+      },
+      opacity: 0,
+      duration: 0.8,
+      x: 250,
+      ease: "Power2.out",
+    });
+
+    gsap.from(Container1Ref.current, {
+      scrollTrigger: {
+        trigger: Container1Ref.current,
+        start: "top 75%",
+      },
+      opacity: 0,
+      duration: 0.8,
+      x: -250,
+      ease: "Power2.out",
+    });
+
+    gsap.from(Heading1Ref.current, {
+      scrollTrigger: {
+        trigger: Heading1Ref.current,
+        start: "top 75%",
+      },
+      y: 200,
+      delay: 0.6,
+      duration: 0.5,
+      ease: "Power2.out",
+    });
+
+    gsap.from(Button1Ref.current, {
+      scrollTrigger: {
+        trigger: Button1Ref.current,
+        start: "top 75%",
+      },
+      y: -200,
+      opacity: 0,
+      delay: 0.6,
+      duration: 0.6,
+      ease: "Power2.out",
+    });
+
+    gsap.from(Data1Ref.current, {
+      scrollTrigger: {
+        trigger: Data1Ref.current,
+        start: "top 75%",
+      },
+      opacity: 0,
+      delay: 0.8,
+      duration: 0.4,
+      ease: "Power2.out",
+    });
+
+    // Section 3
+    gsap.from(illustration2Ref.current, {
+      scrollTrigger: {
+        trigger: illustration2Ref.current,
+        start: "top 75%",
+      },
+      opacity: 0,
+      duration: 0.8,
+      x: -250,
+      ease: "Power2.out",
+    });
+
+    gsap.from(Container2Ref.current, {
+      scrollTrigger: {
+        trigger: Container2Ref.current,
+        start: "top 75%",
+      },
+      opacity: 0,
+      duration: 0.8,
+      x: 250,
+      ease: "Power2.out",
+    });
+
+    gsap.from(Heading2Ref.current, {
+      scrollTrigger: {
+        trigger: Heading2Ref.current,
+        start: "top 75%",
+      },
+      y: 200,
+      delay: 0.6,
+      duration: 0.5,
+      ease: "Power2.out",
+    });
+
+    gsap.from(Button2Ref.current, {
+      scrollTrigger: {
+        trigger: Button2Ref.current,
+        start: "top 75%",
+      },
+      y: -200,
+      opacity: 0,
+      delay: 0.6,
+      duration: 0.6,
+      ease: "Power2.out",
+    });
+
+    gsap.from(Data2Ref.current, {
+      scrollTrigger: {
+        trigger: Data2Ref.current,
+        start: "top 75%",
+      },
+      opacity: 0,
+      delay: 0.8,
+      duration: 0.4,
+      ease: "Power2.out",
+    });
+
+    // Section 4
+    gsap.from(illustration3Ref.current, {
+      scrollTrigger: {
+        trigger: illustration3Ref.current,
+        start: "top 75%",
+      },
+      opacity: 0,
+      duration: 0.8,
+      x: 250,
+      ease: "Power2.out",
+    });
+
+    gsap.from(Container3Ref.current, {
+      scrollTrigger: {
+        trigger: Container3Ref.current,
+        start: "top 75%",
+      },
+      opacity: 0,
+      duration: 0.8,
+      x: -250,
+      ease: "Power2.out",
+    });
+
+    gsap.from(Heading3Ref.current, {
+      scrollTrigger: {
+        trigger: Heading3Ref.current,
+        start: "top 75%",
+      },
+      y: 200,
+      delay: 0.6,
+      duration: 0.5,
+      ease: "Power2.out",
+    });
+
+    gsap.from(Button3Ref.current, {
+      scrollTrigger: {
+        trigger: Button3Ref.current,
+        start: "top 75%",
+      },
+      y: -200,
+      opacity: 0,
+      delay: 0.6,
+      duration: 0.6,
+      ease: "Power2.out",
+    });
+
+    gsap.from(Data3Ref.current, {
+      scrollTrigger: {
+        trigger: Data3Ref.current,
+        start: "top 75%",
+      },
+      opacity: 0,
+      delay: 0.8,
+      duration: 0.4,
+      ease: "Power2.out",
     });
   }, []);
 
@@ -181,7 +365,7 @@ const LandingPage = () => {
                     transform="translate(96 50)"
                     fill="#d1d1d1"
                     stroke="#d1d1d1"
-                    stroke-width="1">
+                    strokeWidth="1">
                     <rect width="14" height="3" rx="1.5" stroke="none" />
                     <rect
                       x="0.5"
@@ -196,7 +380,7 @@ const LandingPage = () => {
                     transform="translate(96 58)"
                     fill="#d1d1d1"
                     stroke="#d1d1d1"
-                    stroke-width="1">
+                    strokeWidth="1">
                     <rect width="32" height="3" rx="1.5" stroke="none" />
                     <rect
                       x="0.5"
@@ -211,7 +395,7 @@ const LandingPage = () => {
                     transform="translate(114 66)"
                     fill="#d1d1d1"
                     stroke="#d1d1d1"
-                    stroke-width="1">
+                    strokeWidth="1">
                     <rect width="14" height="3" rx="1.5" stroke="none" />
                     <rect
                       x="0.5"
@@ -226,9 +410,15 @@ const LandingPage = () => {
               </svg>
             </div>
             <div className="flex pl-28 justify-around basis-3/4">
-              <SLink to="data" smooth={true} duration={700}><div>DataStructures</div></SLink>
-              <SLink to="algorithm" smooth={true} duration={1400}><div>Algorithm</div></SLink>
-              <SLink to="about" smooth={true} duration={2100}><div>About</div></SLink>
+              <SLink to="data" smooth={true} duration={700}>
+                <div>DataStructures</div>
+              </SLink>
+              <SLink to="algorithm" smooth={true} duration={1400}>
+                <div>Algorithm</div>
+              </SLink>
+              <SLink to="about" smooth={true} duration={2100}>
+                <div>About</div>
+              </SLink>
             </div>
           </div>
           <div className="basis-1/2 flex justify-end pr-28">
@@ -256,7 +446,7 @@ const LandingPage = () => {
           </div>
           <div className="z-50 mix-blend-screen" ref={lineRef}>
             <svg height="2" width="350">
-              <line x1="25" x2="300" stroke="#b6b6b6" stroke-width="4" />
+              <line x1="25" x2="300" stroke="#b6b6b6" strokeWidth="4" />
             </svg>
           </div>
           <div className="text-7xl ml-28 z-20 mix-blend-screen" ref={oneRef}>
@@ -296,24 +486,28 @@ const LandingPage = () => {
       </div>
       <div className="bg-[#041C32] h-screen w-screen relative" id="data">
         <div className="flex h-full w-full">
-          <div className="basis-1/2 relative">
+          <div className="basis-1/2 relative" ref={illustration1Ref}>
             <img
               src={illustration1}
               alt="Illustration"
               className="absolute w-9/12 bottom-24 left-24 object-cover"
             />
           </div>
-          <div className="basis-1/2 relative">
+          <div className="basis-1/2 relative" ref={Container1Ref}>
             <div className="absolute w-full h-3/4 top-24 flex flex-col">
-              <div className="text-9xl pt-4 pl-24">DATA</div>
-              <div className="text-7xl self-end p-4 pr-24">STRUCTURE</div>
-              <div className=" w-3/4 self-center p-4 text-xl">
+              <div className="flex flex-col" ref={Heading1Ref}>
+                <div className="text-9xl pt-4 pl-24">DATA</div>
+                <div className="text-7xl self-end p-4 pr-24">STRUCTURE</div>
+              </div>
+              <div className=" w-3/4 self-center p-4 text-xl" ref={Data1Ref}>
                 Data Structure is a way to store and organize data so that it
                 can be used efficiently. Our Data Structure tutorial includes
                 all important topics of Data Structure such as Array, Trees,
                 Linked List, Stack, Queue, Graph etc.
               </div>
-              <div className=" w-fit p-4 rounded-full bg-[#FFDD00] text-gray-900 text-2xl self-end mr-32 cursor-pointer">
+              <div
+                className=" w-fit p-4 rounded-full bg-[#FFDD00] text-gray-900 text-2xl self-end mr-32 cursor-pointer"
+                ref={Button1Ref}>
                 Learn More
               </div>
             </div>
@@ -322,21 +516,25 @@ const LandingPage = () => {
       </div>
       <div className="bg-[#041C32] h-screen w-screen relative" id="algorithm">
         <div className="flex h-full w-full">
-          <div className="basis-1/2 relative">
-            <div className="absolute w-full h-3/4 top-24 flex flex-col">
-              <div className="text-8xl pt-4 pl-24">ALGORITHM</div>
-              <div className=" w-3/4 self-center p-4 text-xl">
+          <div className="basis-1/2 relative" ref={Container2Ref}>
+            <div className="absolute w-full h-3/4 top-44 flex flex-col">
+              <div className="text-8xl pt-4 pl-24" ref={Heading2Ref}>
+                ALGORITHM
+              </div>
+              <div className=" w-3/4 self-center p-4 mt-8 text-xl" ref={Data2Ref}>
                 Algorithm is a collection of steps to solve a particular
                 problem. Our Algorithm tutorial includes mostly used algorithm
                 design techniques such as Greedy, Divide & Conquer, Dynamic
                 programming, Backtracking etc.
               </div>
-              <div className=" w-fit p-4 rounded-full bg-[#FFDD00] text-gray-900 text-2xl self-start ml-28 mt-5 cursor-pointer">
+              <div
+                className=" w-fit p-4 rounded-full bg-[#FFDD00] text-gray-900 text-2xl self-start ml-28 mt-8 cursor-pointer"
+                ref={Button2Ref}>
                 Learn More
               </div>
             </div>
           </div>
-          <div className="basis-1/2 relative">
+          <div className="basis-1/2 relative" ref={illustration2Ref}>
             <img
               src={illustration2}
               alt="Illustration"
@@ -347,18 +545,20 @@ const LandingPage = () => {
       </div>
       <div className="bg-[#041C32] h-screen w-screen relative" id="about">
         <div className="flex h-full w-full">
-          <div className="basis-1/2 relative">
+          <div className="basis-1/2 relative" ref={illustration3Ref}>
             <img
               src={illustration3}
               alt="Illustration"
-              className="absolute w-9/12 top-24 left-24 object-cover"
+              className="absolute w-9/12 top-12 left-24 object-cover"
             />
           </div>
-          <div className="basis-1/2 relative">
-            <div className="absolute w-full h-3/4 top-24 flex flex-col">
-              <div className="text-9xl pt-4 pl-24">ABOUT</div>
-              <div className="text-7xl self-end p-4 pr-24">PROJECT</div>
-              <div className=" w-3/4 self-center p-4 text-xl">
+          <div className="basis-1/2 relative" ref={Container3Ref}>
+            <div className="absolute w-full h-3/4 top-12 flex flex-col">
+              <div className="flex flex-col" ref={Heading3Ref}>
+                <div className="text-9xl pt-4 pl-24">ABOUT</div>
+                <div className="text-7xl self-end p-4 pr-24">PROJECT</div>
+              </div>
+              <div className=" w-3/4 self-center p-4 text-xl" ref={Data3Ref}>
                 Data Structure is a way to store and organize data so that it
                 can be used efficiently. Our Data Structure tutorial includes
                 all important topics of Data Structure such as Array, Trees,
@@ -368,7 +568,8 @@ const LandingPage = () => {
                 className=" w-fit p-4 rounded-full bg-[#FFDD00] text-gray-900 text-2xl self-end mr-32 cursor-pointer"
                 onClick={() => {
                   window.location.href = "https://www.google.com/";
-                }}>
+                }}
+                ref={Button3Ref}>
                 Visit Github
               </div>
             </div>
