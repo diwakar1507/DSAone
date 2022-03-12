32156114075144d98c4f31fc1c4e8e2f2916ce64
Ishan Shukla <ishanshukla05@gmail.com>
2022-03-12 19:06:02 +0530

    ViewCard Update

--- src/Components/ViewCard.js
+++ src/Components/ViewCard.js
@@ -15,16 +15,3 @@ function ViewCard(props) {
 }
 
 export default ViewCard;      
-
-{/* <div className={`relative ${props.index % 2 ? 'bg-plain-white' : 'bg-primary-3'} flex flex-col rounded-lg ml-auto mr-auto h-64 w-64 shadow-lg transform transition duration-500 hover:scale-110 hover:shadow-xl`}>
-<div className="h-full ">
-  <div className="relative h-full">
-    <img src={props.image} alt="To be Added" className="h-3/4 p-4 absolute top-16 "></img>
-  </div>
-</div>
-<div className="z-20 absolute top-0 h-full w-full">
-  <div className="flex flex-col h-full w-full justify-center items-center">
-    <div className="flex-1 p-4 text-3xl font-Hanseif place-self-start">{props.index}. {props.name}</div>
-  </div>
-</div>
-</div> */}
