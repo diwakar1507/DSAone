9567bd3a5ef070b3c2d2707e43991a4bb558d4df
Ishan Shukla <ishanshukla05@gmail.com>
2022-03-12 11:52:17 +0530

    Topbar tweak

--- src/pages/DataStructurePage/NewMainDS.js
+++ src/pages/DataStructurePage/NewMainDS.js
@@ -5,7 +5,7 @@ const NewMainDS = () => {
   return (
     <div className="bg-primary-1 h-screen w-screen relative">
       <div className="fixed top-0 w-screen z-30">
-        <div className="backdrop-filter shadow-bar backdrop-blur-sm">
+        <div className="backdrop-filter shadow-bar backdrop-blur-md backdrop-brightness-125 shadow-2xl">
           <div className="flex p-8">
             <div className="flex-1 text-white">Data Structure</div>
           </div>
